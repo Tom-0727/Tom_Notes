770544378b71a862fc25bf206238c23547f6df47
Tom-0727 <lliu034@e.ntu.edu.sg>
2023-08-03 18:35:33 +0800

    Transformer Debug

--- transformers/transformer/transformer.py
+++ transformers/transformer/transformer.py
@@ -25,32 +25,42 @@ class EncodeBlock(nn.Module):
         self.dropout2 = nn.Dropout(0.1)
 
     def forward(self, key, query, value):
-        attention_out = self.attention(key, query, value)
-        attention_res_out = attention_out + value
-        norm1_out = self.dropout1(self.norm1(attention_res_out))
+        # Multi-head Attention
+        attention_out = self.attention(key = key, 
+                                       query = query, 
+                                       value = value)
+        
+        # Notice that you need to add the query but not K/V because when you make Cross Attention the opt would be in size of query 
+        attention_res_out = attention_out + query  # Residual connection
+        norm1_out = self.dropout1(self.norm1(attention_res_out))  # Normalization & DropOut
 
         ff_out = self.feed_forward(norm1_out)
-        ff_res_out = ff_out + norm1_out
-        norm2_out = self.dropout2(self.norm2(ff_res_out))
+        ff_res_out = ff_out + norm1_out  # Residual connection
+        norm2_out = self.dropout2(self.norm2(ff_res_out))  # Normalization & DropOut
 
         return norm2_out
 
 
 class Encoder(nn.Module):
-    def __init__(self, seq_len, vocab_size, embed_dim, num_layers=6, expansion_factor=4, n_heads=8):
+    def __init__(self, max_seq_len, vocab_size, embed_dim, num_layers=6, expansion_factor=4, n_heads=8):
         super(Encoder, self).__init__()
 
-        self.embedding_layer = Embedding(vocab_size, embed_dim)
-        self.positional_encoder = PositionalEmbedding(seq_len, embed_dim)
+        self.embedding_layer = Embedding(vocab_size, embed_dim)  
+        self.positional_encoder = PositionalEmbedding(max_seq_len, embed_dim)
 
         self.layers = nn.ModuleList([EncodeBlock(embed_dim, expansion_factor, n_heads) for _ in range(num_layers)])
         self.dropout = nn.Dropout(0.1)
     def forward(self, x):
+
+        # Word Embedding + Positional Encoding
         embed_out = self.embedding_layer(x)
         out = self.positional_encoder(embed_out)
         out = self.dropout(out)
+
         for layer in self.layers:
-            out = layer(out, out, out)
+            out = layer(key = out, 
+                        query = out, 
+                        value = out)
         
         return out
 
@@ -67,20 +77,26 @@ class DecoderBlock(nn.Module):
     def forward(self, key, value, x, mask):
 
         # Only pass mask to the first attention block
-        attention = self.attention(x, x, x, mask)
+        attention = self.attention(key = x, 
+                                   query = x, 
+                                   value = x, 
+                                   mask = mask)
         query = self.dropout(self.norm(attention + x))
 
-        out = self.transformer_block(key, value, query)
+        # Cross Attention
+        out = self.transformer_block(key = key, 
+                                     query = query, 
+                                     value = value)
 
         return out
     
 
 class Decoder(nn.Module):
-    def __init__(self, t_vocab_size, embed_dim, seq_len, num_layers=6, expansion_factor=4, n_heads=8):
+    def __init__(self, t_vocab_size, embed_dim, max_seq_len, num_layers=6, expansion_factor=4, n_heads=8):
         super(Decoder, self).__init__()
 
         self.word_embedding = Embedding(t_vocab_size, embed_dim)
-        self.pos_embedding = PositionalEmbedding(seq_len, embed_dim)
+        self.pos_embedding = PositionalEmbedding(max_seq_len, embed_dim)
         self.layers = nn.ModuleList(
             [
                 DecoderBlock(embed_dim, expansion_factor, n_heads)
@@ -98,21 +114,24 @@ class Decoder(nn.Module):
         x = self.dropout(x)
 
         for layer in self.layers:
-            x = layer(encoder_out, encoder_out, x, mask)  # dropout had made in sub_layer
+            x = layer(key = encoder_out, 
+                      x = x, 
+                      value = encoder_out, 
+                      mask = mask)  # dropout had made in sub_layer
 
-        out = F.softmax(self.fc_out(x))
+        out = F.softmax(self.fc_out(x), dim=-1)
         return out
     
 
 class Transformer(nn.Module):
-    def __init__(self, embed_dim, s_vocab_size, t_vocab_size, seq_len, 
+    def __init__(self, embed_dim, s_vocab_size, t_vocab_size, max_seq_len, 
                  num_layers = 6, 
                  expansion_factor = 4,
                  n_heads = 8):
         super(Transformer, self).__init__()
 
         self.t_vocab_size = t_vocab_size
-        self.encoder = Encoder(seq_len = seq_len, 
+        self.encoder = Encoder(max_seq_len = max_seq_len, 
                                vocab_size = s_vocab_size, 
                                embed_dim = embed_dim, 
                                num_layers = num_layers, 
@@ -120,7 +139,7 @@ class Transformer(nn.Module):
                                n_heads = n_heads)
         self.decoder = Decoder(t_vocab_size = t_vocab_size, 
                                embed_dim = embed_dim, 
-                               seq_len = seq_len, 
+                               max_seq_len = max_seq_len, 
                                num_layers = num_layers, 
                                expansion_factor = expansion_factor, 
                                n_heads = n_heads)
@@ -130,26 +149,6 @@ class Transformer(nn.Module):
         batch_size, trg_len = trg.shape
         trg_mask = torch.tril(torch.ones((trg_len, trg_len))).expand(batch_size, 1, trg_len, trg_len)
         return trg_mask
-    
-    # For inference
-    def decode(self, src, trg):
-        trg_mask = self.make_trg_mask(trg)
-        encoder_out = self.encoder(src)
-        out_labels = []
-        # batch_size, seq_len = src.shape[0], src.shape[1]
-        seq_len = trg.shape[1]
-
-        out = trg
-        for i in range(seq_len):
-            out = self.decoder(out, encoder_out, trg_mask)
-            
-            # take the last token
-            out = out[:,-1,:]
-            out = out.argmax(-1)
-            out_labels.append(out.item())
-            out = torch.unsqueeze(out, axis=0)
-        
-        return out_labels
     
     # For training
     def forward(self, src, trg):

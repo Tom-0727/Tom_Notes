153e8a249eb635d9052f5a1ee1e82ba4bae1ef7e
Tom-0727 <lliu034@e.ntu.edu.sg>
2023-08-03 18:45:03 +0800

    Translator Develop

---
+++ transformers/transformer/translater.py
@@ -0,0 +1,15 @@
+from .transformer import *
+
+
+class E2VTranslater:
+    def __init__(self):
+        # Tokenier
+
+        # Model
+        pass
+
+    def translate(self, en_text: str):
+        # Tokenization
+
+        # Translation
+        pass
